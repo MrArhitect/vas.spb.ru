9415a7ccc8a5920c7d4f9c64f0b55fae410751d1
MrArhitect <292359582+MrArhitect@users.noreply.github.com>
2026-09-25 13:57:10 +0300

    v.1.0.2 - (Подготовка к публикации в GitHub Pages)

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
+  base: '/vas.spb.ru/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
